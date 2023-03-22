6b7c3b1532518d116a3d6ba02310e6cdbee4d490
pavelstehov <sc0utpsyx@gmail.com>
2023-03-22 23:39:56 +0300

    Домашнее задание.

--- api/src/main/java/ru/geekbrains/march/market/api/CartItemDto.java
+++ api/src/main/java/ru/geekbrains/march/market/api/CartItemDto.java
@@ -9,6 +9,14 @@ public class CartItemDto {
     private BigDecimal pricePerProduct;
     private BigDecimal price;
 
+    private CartItemDto(Builder builder) {
+        setProductId(builder.productId);
+        setProductTitle(builder.productTitle);
+        setQuantity(builder.quantity);
+        setPricePerProduct(builder.pricePerProduct);
+        setPrice(builder.price);
+    }
+
     public Long getProductId() {
         return productId;
     }
@@ -52,11 +60,44 @@ public class CartItemDto {
     public CartItemDto() {
     }
 
-    public CartItemDto(Long productId, String productTitle, int quantity, BigDecimal pricePerProduct, BigDecimal price) {
-        this.productId = productId;
-        this.productTitle = productTitle;
-        this.quantity = quantity;
-        this.pricePerProduct = pricePerProduct;
-        this.price = price;
+
+    public static final class Builder {
+        private Long productId;
+        private String productTitle;
+        private int quantity;
+        private BigDecimal pricePerProduct;
+        private BigDecimal price;
+
+        public Builder() {
+        }
+
+        public Builder withProductId(Long val) {
+            productId = val;
+            return this;
+        }
+
+        public Builder withProductTitle(String val) {
+            productTitle = val;
+            return this;
+        }
+
+        public Builder withQuantity(int val) {
+            quantity = val;
+            return this;
+        }
+
+        public Builder withPricePerProduct(BigDecimal val) {
+            pricePerProduct = val;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal val) {
+            price = val;
+            return this;
+        }
+
+        public CartItemDto build() {
+            return new CartItemDto(this);
+        }
     }
 }
--- cart-service/src/main/java/ru/geekbrains/march/market/cart/converters/CartItemConverter.java
+++ cart-service/src/main/java/ru/geekbrains/march/market/cart/converters/CartItemConverter.java
@@ -7,6 +7,12 @@ import ru.geekbrains.march.market.cart.utils.CartItem;
 @Component
 public class CartItemConverter {
     public CartItemDto entityToDto(CartItem c) {
-        return new CartItemDto(c.getProductId(), c.getProductTitle(), c.getQuantity(), c.getPricePerProduct(), c.getPrice());
+        return new CartItemDto.Builder()
+                .withProductId(c.getProductId())
+                .withProductTitle(c.getProductTitle())
+                .withPrice(c.getPrice())
+                .withPricePerProduct(c.getPricePerProduct())
+                .withQuantity(c.getQuantity())
+                .build();
     }
 }
